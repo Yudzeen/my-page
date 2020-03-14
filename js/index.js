feeb6243cc289cf752eae85bed5f3efa7c9d16e8
Yudzeen <eugenejavinas@gmail.com>
2020-03-14 15:29:24 +0800

    Add button
    Signed-off-by: Yudzeen <eugenejavinas@gmail.com>

---
+++ js/index.js
@@ -0,0 +1,9 @@
+window.onload = function () {
+    document.getElementById("click_me_button").onclick = function() {
+        alert("Yujin Pogi");
+    };
+}
+
+const alert = function(text) {
+    window.alert(text);
+};
