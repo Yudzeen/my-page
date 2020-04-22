5d624295c61fff452ddb90151727b3d07641681d
Yudzeen <eugenejavinas@gmail.com>
2020-04-22 21:27:49 +0800

    Update contents
    Changes:
    - Changed text
    - Dark mode support
    
    Signed-off-by: Yudzeen <eugenejavinas@gmail.com>

--- js/index.js
+++ js/index.js
@@ -1,9 +1,3 @@
-window.onload = function () {
-    document.getElementById("click_me_button").onclick = function() {
-        alert("Yujin Pogi");
-    };
-}
-
 const alert = function(text) {
     window.alert(text);
 };
