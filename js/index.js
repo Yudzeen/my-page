ba114435fbe60ebd0677695a55e762ee76350af2
Yudzeen <eugenejavinas@gmail.com>
2020-05-22 17:44:31 +0800

    Add info js interface
    Signed-off-by: Yudzeen <eugenejavinas@gmail.com>

--- js/index.js
+++ js/index.js
@@ -1,3 +1,10 @@
+var info = {};
+
 const alert = function(text) {
     window.alert(text);
 };
+
+const updateInfo = function(info) {
+    info = JSON.parse(info);
+    console.log("Update info: %O", info);
+}
