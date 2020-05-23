2971bd1e3120e6e20afb9114c235bd1eccc163f1
Yudzeen <eugenejavinas@gmail.com>
2020-05-23 17:07:12 +0800

    Fix variable name
    Signed-off-by: Yudzeen <eugenejavinas@gmail.com>

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ const alert = function(text) {
     window.alert(text);
 };
 
-const updateInfo = function(info) {
-    info = JSON.parse(info);
+const updateInfo = function(newInfo) {
+    info = JSON.parse(newInfo);
     console.log("Update info: %O", info);
 }
